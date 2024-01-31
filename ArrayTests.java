cbdbcb32bd8d404e5b903d7b8060a8379a3e5c87
drcano <156254264+drcano@users.noreply.github.com>
2024-01-31 15:32:15 -0800

    updated

--- ArrayTests.java
+++ ArrayTests.java
@@ -5,14 +5,22 @@ public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int[] testArr = {0,1,2,3,4,5}; 
+    int[] rev = {5,4,3,2,1,0}; 
     ArrayExamples.reverseInPlace(input1);
+    ArrayExamples.reverseInPlace(testArr);
     assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(testArr, rev);
 	}
 
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int[] testArr = {0,1,2,3,4,5}; 
+    int[] rev = {5,4,3,2,1,0};
+    testArr = ArrayExamples.reversed(testArr); 
+    assertArrayEquals(testArr, rev);
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
 }
